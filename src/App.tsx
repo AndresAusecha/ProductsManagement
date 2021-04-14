d17361b45ea4d4f1e958712abc8d58a21d99280e
andres.ausecha <andres@aamsis.co>
2021-04-13 21:19:15 -0500

    add styles and implement redux management correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
-import { useAppSelector } from './store/hooks';
-import { Product } from './store/productsSlice';
+import { useAppSelector, useAppDispatch } from './store/hooks';
+import { add, Product } from './store/productsSlice';
 
 function App() {
   const products = useAppSelector((state) => state.products.products);
-  console.log(products);
+  const [idValue, setIdValue] = useState(0);
+  const [nameValue, setNameValue] = useState('');
+  const dispatch = useAppDispatch();
+
+  const onClickHandler = () => {
+    const newProduct: Product = { id: idValue, name: nameValue };
+    dispatch(
+      add(newProduct)
+    );
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <h3>
           Products management system
         </h3>
-      </header>
-      <div>
-        {products.map((prod: Product) => <p>{prod.name}</p>)}
+      </div>
+      <div className="App-body">
+      <div className="App-body-products-form">
+        <p className="App-body-products-form-heading">Products form</p>
+        <div className="App-body-products-form-fields">
+          <input type="number" placeholder="Id" onChange={({ target: { value } }) => setIdValue(parseInt(value)) } />
+          <input type="text" placeholder="Name" onChange={({ target: { value } }) => setNameValue(value) } />
+          <div className="App-body-products-form-fields-submit-container">
+            <button className="submit" disabled={idValue === 0 || nameValue === ''} onClick={onClickHandler}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+        <div className="App-body-products-table">
+          <p className="App-body-products-table-heading">Products table</p>
+          {products.map((prod: Product) => (
+            <div className="App-body-products-table-row">
+              <p><b>Id:</b> {prod.id}</p>
+              <p> - </p>
+              <p><b>Name:</b> {prod.name}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
